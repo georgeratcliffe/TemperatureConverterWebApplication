d1c0323cb4a8c51b7149bd2311d9221c499baa96
GEORGE W RATCLIFFE <george70fs@outlook.com>
2022-12-03 10:27:37 +0000

    Add project files.

---
+++ TemperatureConverterWebApplication/Api/TempController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+
+namespace TemperatureConverterWebApplication.Api
+{
+    [Route("home/api/[controller]")]
+    [ApiController]
+    public class TempController : ControllerBase
+    {
+        /// <summary>
+        /// Returns series of Celsius/Fahrenheit values to be consumed by DataTable jQuery Javascript library
+        /// </summary>
+        [HttpGet]
+        [Route("GetTableData")]
+        public async Task<string> Get()
+        {
+            return await Task.Run(() => GenerateCtoF());
+        }
+
+
+        private string GenerateCtoF()
+        {
+            StringBuilder sb = new StringBuilder("{\"data\": [");
+
+            for (int x = 0; x <= 100; x++)
+            {
+                decimal far = (Decimal.Divide(9, 5) * x) + 32;
+                string row = $"[{x},{far}],";
+                sb.Append(row);
+            }
+
+            sb.Remove(sb.Length - 1, 1);
+            sb.Append("]}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TemperatureConverterWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TemperatureConverterWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
